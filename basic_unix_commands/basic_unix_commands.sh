31e952c10445fa7ffd5326f84211b4f5a9171847
Suraj Kumar <surajkumar@Surajs-MacBook-Air.local>
2019-09-27 17:30:38 +0530

    Added a file for basic unix commands

---
+++ basic_unix_commands/basic_unix_commands.sh
@@ -0,0 +1,27 @@
+# Show the current directory where I am.
+pwd
+
+
+
+# Show the contents of the current directory.
+ls -l
+
+
+
+# This is relative from the current directory.
+cd path/to/new/directory
+
+
+
+# Move to the parent directory
+cd ..
+
+
+
+# If you want to go to the basic_unix_commands directory
+cd ~/Development/unix_utilities/basic_unix_commands/
+
+
+
+# If you want to go to the python directory
+cd ~/Development/python/
